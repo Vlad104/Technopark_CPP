6094cf73124cfda457bcc3bf459ac3dcaba75412
Vlad104 <vlad104da@gmail.com>
2018-09-26 19:09:12 +0300

    step3

--- dz1.c
+++ dz1.c
@@ -7,16 +7,21 @@ void foo( char *str[], int n ) {
 
 int main() {
 	char * line = (char *)malloc(sizeof(char));
+	char ** lines;
 	char sym;
 	int i = 0;
-	while ( (sym = getchar()) != '\n') {
-		line[i] = sym;
-		i++;
-		line = (char *) realloc(line, i*sizeof(char));
+	while ( sym = getchar() != EOF ) {
+		int j = 0;
+		while ( (sym = getchar()) != '\n') {
+			line[j] = sym;
+			j++;
+			line = (char *) realloc(line, j*sizeof(char));
+		}
+		line[j] = '\0';
+		*lines[i] = (char *) malloc((j+1)*sizeof(char)); ///ss
+		//printf("%s\n", line);
+		free(line);
 	}
-	line[i] = '\0';
-	printf("%s\n", line);
-	free(line);
 	getchar();	
 	return 0;
 }

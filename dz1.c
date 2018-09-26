06845599481c3bcb586089ce0bb16d700c9d9ea8
Vlad104 <vlad104khk@gmail.com>
2018-09-26 17:40:00 +0300

    fc1

---
+++ dz1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void foo( char *str[], int n ) {
+	
+}
+
+int main() {
+	char * line = (char *)malloc(sizeof(char));
+	char sym;
+	int i = 0;
+	while ( (sym = getchar()) != '\n') {
+		line[i] = sym;
+		i++;
+		line = (char *) realloc(line, i*sizeof(char));
+	}
+	line[i] = '\0';
+	printf("%s\n", line);
+	free(line);
+	getchar();	
+	return 0;
+}

77ae65a8d4f843906e1e75c8911454cffe99e2a7
Vlad104 <vlad104da@gmail.com>
2018-09-27 14:38:38 +0300

    step4

--- dz1.c
+++ dz1.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <string.h>
 
-void foo( char *str[], int n ) {
-	////sada
+void foo( char *line, int n ) {
+	//char * result = (char *)malloc(n*sizeof(char));
+	//memcpy(result, line, n*sizeof(char));
+	char * result = (char *)malloc(sizeof(char));
+	int i = 0, j =0;
+	while (i < n) {
+		if ( i > 0 && line[i] == ' ' && line[i-1] == ' ') {
+			i++;
+		}
+		else {
+			result = (char *)realloc(result, (j+1)*sizeof(char));
+			result[j] = line[i];
+			i++;
+			j++;
+		}
+	}
+	result[j] = '\0';
+	printf("result = %s\n", result);
+	return;
 }
 
 int main() {
-	char * line = (char *)malloc(sizeof(char));
-	char ** lines;
-	char sym;
-	int i = 0;
-	while ( sym = getchar() != EOF ) {
-		int j = 0;
+	while ( 1 == 1 ) {
+		char * line = (char *)malloc(sizeof(char));
+		char sym;
+		int i = 0;
+		
 		while ( (sym = getchar()) != '\n') {
-			line[j] = sym;
-			j++;
-			line = (char *) realloc(line, j*sizeof(char));
+			line[i] = sym;
+			i++;
+			line = (char *) realloc(line, i*sizeof(char));
 		}
-		line[j] = '\0';
-		*lines[i] = (char *) malloc((j+1)*sizeof(char)); ///ss
-		//printf("%s\n", line);
+		line[i] = '\0';
+		printf("input  = %s\n", line);
+		foo(line, i);
 		free(line);
 	}
+	
 	getchar();	
 	return 0;
 }

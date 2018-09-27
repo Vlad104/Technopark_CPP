8021e47aeb189e00417a5bc1dffa8eefae46fb2c
Vlad104 <vlad104da@gmail.com>
2018-09-27 23:16:59 +0300

    step7

--- dz1.c
+++ dz1.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <errno.h>
 
-char * foo( const char * line, int n ) {
+char * deleteMultiSpace( const char * line, int n ) {
 	char * result = (char *)malloc(sizeof(char));
+	if (errno == ENOMEM) {
+		printf("[error");
+		return 0;
+	}
 	int i = 0, j =0;
 	while (i < n) {
 		if ( i > 0 && line[i] == ' ' && line[i-1] == ' ') {
@@ -12,6 +16,7 @@ char * foo( const char * line, int n ) {
 		else {
 			result = (char *)realloc(result, (j+1)*sizeof(char));
 			if (errno == ENOMEM) {
+				printf("[error");
 				return 0;
 			}
 			result[j] = line[i];
@@ -25,9 +30,10 @@ char * foo( const char * line, int n ) {
 
 int main() {
 	errno = 0;
-	while ( 1 == 1 ) {
+	while ( sym != EOF ) {
 		char * line = (char *)malloc(sizeof(char));
 		if (errno == ENOMEM) {
+			printf("[error");
 			return 0;
 		}
 		char sym;
@@ -36,26 +42,22 @@ int main() {
 		while ( (sym = getchar()) != '\n') {
 			line = (char *) realloc(line, (i+1)*sizeof(char));
 			if (errno == ENOMEM) {
+				printf("[error");
 				return 0;
 			}
 			line[i] = sym;
 			i++;
-		}
-		
-		/*
-		char * line;
-		scanf("%[^\n]%s", line);
-		int i = sizeof(line) / sizeof(char);
-		*/
-		
+		}		
+	
 		line[i] = '\0';
-		char * result = foo(line, i);
+		char * result = deleteMultiSpace(line, i);
 		
 		printf("input  = %s\n", line);
 		printf("result = %s\n", result);
 		
 		//int nbytes = printf("%s\n", result);
 		//if (errno == EILSEQ || errno == EINVAL) {
+		//	printf("[error");
 		//	return 0;
 		//}
 		

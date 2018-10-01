2c9a77f37661f1769a143c260620a2993fb3a8aa
Vlad104 <vlad104khk@gmail.com>
2018-10-01 14:34:28 +0300

    dz2 in progress

--- dz2.c
+++ dz2.c
@@ -11,7 +11,7 @@ float make_digit(const char * line, size_t *p_str_index) {
 		str_index++;
 	}
 	char * temp = (char * ) malloc( (str_index - old_str_index) * sizeof(char));
-	for (size_t i = old_str_index; i < str_index; i++) {
+	for (size_t i = 0; i < str_index - old_str_index; i++) {
 		temp[i] = line[old_str_index + i];
 	}
 	float digit = atof(temp);
@@ -21,7 +21,10 @@ float make_digit(const char * line, size_t *p_str_index) {
 }
 
 float polyak(const char * line) {
-	float stack[1000];
+	float stack[10];
+	for (int i = 0; i < 10; i++) {
+		stack[i] = 0.0;
+	}
 	size_t stack_index = 0;	
 	size_t str_index = 0;
 	while (line[str_index] != '\0') {
@@ -29,6 +32,7 @@ float polyak(const char * line) {
 		switch (symbol) {
 			case ' ':
 				str_index++;
+				break;
 			case '=':
 				return stack[stack_index - 1];
 				stack_index--;
@@ -56,13 +60,47 @@ float polyak(const char * line) {
 				break;
 			default:
 				stack[stack_index] = make_digit(line, &str_index);
+				stack_index++;
 				break;
 		}
+
+		/*
+		printf("sym = %c, ind = %ld, stack = %ld\n", symbol, str_index, stack_index);
+		for (int i = 0; i < 10; i++) {
+			printf("%.2f ", stack[i]);
+		}
+		printf("\n");
+		*/
+
 	}
 	return stack[stack_index - 1];
 }
 
+char * make_polish(const char * line) {
 
+	char * result = (char *) malloc(sizeof(char));
+	char * temp   = (char *) malloc(sizeof(char));
+	size_t line_index = 0;
+	size_t res_index  = 0;
+	size_t temp_index = 0;
+	while (line[line_index] != '\0') {
+		if (isdigit[line[line_index]] || line[line_index] == '.') {
+			result[res_index] = line[line_index];
+			res_index++;
+		} else if (line[line_index] != ' ') {
+			if (line[line_index] == ')') {
+				result[res_index] = temp[temp_index-1];
+				res_index++;				
+			} else if (line[line_index] != '(') {
+				temp[temp_index] = line[line_index];
+				temp_index++;					
+			}		
+		}		
+		line_index++;
+	}
+
+	return result;
+}
 
 
 int main() {
@@ -71,6 +109,7 @@ int main() {
 	size_t size = 0;
 	ssize_t bytes = getline(&line, &size, stdin);
 
+	//new_line = make_polish(line);
 	float result = polyak(line);
 	printf("%.2f\n", result);
 

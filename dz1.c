f6e884ac5991cdc1f1aabebd996c90eb980eecdb
Vlad104 <vlad104da@gmail.com>
2018-10-05 23:46:43 +0300

    aaa

--- dz1.c
+++ dz1.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <errno.h>
 
-// function delete multi spaces
+// удаление лишних пробелов
 char ** deleteMultiSpace( char * const * const lines, size_t str_num ) {
 	char ** result = (char **) malloc(str_num*sizeof(char*));
-	if (errno == ENOMEM) { // if malloc error then close programm
+	if (errno == ENOMEM) { // в случае ошибки освоболить выделенную память, вернуть нулевой указатель
 		for (int i = 0; i < str_num; i++) {
 			free(result[i]);
 		}
@@ -15,9 +15,9 @@ char ** deleteMultiSpace( char * const * const lines, size_t str_num ) {
 	}
 
 	for (size_t i = 0; i < str_num; i++) {
-		size_t str_len = 0, new_str_len = 0;  // str_len index of input array, new_str_len index of output array, 
+		size_t str_len = 0, new_str_len = 0;  // str_len - индекс входной строки, new_str_len индекс новой строки, 
 		result[i] = (char *)malloc(sizeof(char));
-		if (errno == ENOMEM) {
+		if (errno == ENOMEM) { 
 			for (int i = 0; i < str_num; i++) {
 				free(result[i]);
 			}
@@ -25,12 +25,12 @@ char ** deleteMultiSpace( char * const * const lines, size_t str_num ) {
 			return NULL;
 		}
 		while (lines[i][str_len] != '\0') {
-			// if symbol is space and previous symboll is space too
+			// если текущий и предыдущий символы пробелы переходим на следующий символ входной последовательности
 			if ( str_len > 0 && lines[i][str_len] == ' ' && lines[i][str_len-1] == ' ') {
 				str_len++;
 			}
 			else { 
-				// copy other symbols
+				// копирование остальных символов
 				result[i][new_str_len] = lines[i][str_len];
 				str_len++;
 				new_str_len++;
@@ -44,7 +44,6 @@ char ** deleteMultiSpace( char * const * const lines, size_t str_num ) {
 				}
 			}
 		}
-		//printf("%d %d\n", str_len, new_str_len);
 		result[i][new_str_len] = '\0';
 	}
 	return result;
@@ -52,23 +51,24 @@ char ** deleteMultiSpace( char * const * const lines, size_t str_num ) {
 
 int main(int argc, char *argv[]) {
 	errno = 0;
-	char ** lines = (char **) malloc(sizeof(char*)); // pointer to string array
-	if (errno == ENOMEM) { // if malloc error then close programm
+	char ** lines = (char **) malloc(sizeof(char*));
+	if (errno == ENOMEM) { // в случае ошибки выделения памяти - освободить память, завершить программу
 		//free(lines[0]);
 		free(lines);
 		printf("[error]");
 		return 0;
 	}
 
-	size_t str_num = 0;	// numbers of string
+	size_t str_num = 0;	// кол-во строк
 	ssize_t bytes = 0;
 
-	// start reading input strings
+	// цикл чтения массива строк
 	do {
 		lines[str_num] = NULL;
 		size_t size = 0;
-		bytes = getline(&lines[str_num], &size, stdin);
-		if ( errno == EINVAL || errno == ENOMEM) {		
+		bytes = getline(&lines[str_num], &size, stdin); // считывание вхдной строки, getline динамически выделяет буфер кратный 120 байтам
+		printf("%d", size);
+		if ( errno == EINVAL || errno == ENOMEM) {	
 			printf("[error]");
 			for (int i = 0; i < str_num; i++) {
 				free(lines[i]);
@@ -78,7 +78,7 @@ int main(int argc, char *argv[]) {
 		}
 		
 		str_num++;
-		lines = (char **) realloc(lines, (str_num + 1) * sizeof(char*));
+		lines = (char **) realloc(lines, (str_num + 1) * sizeof(char*)); // реаллоцирование памяти для следующей строки
 		if (errno == ENOMEM) {
 			for (int i = 0; i < str_num; i++) {
 				free(lines[i]);
@@ -89,10 +89,10 @@ int main(int argc, char *argv[]) {
 		}
 
 	} while (bytes > 0);
-	str_num--;
+	str_num--; // так как в цикле инкрементируется лишний раз
 
-	char **result = deleteMultiSpace(lines, str_num); // return pointer to modificated string array
-	if (result == NULL) {
+	char **result = deleteMultiSpace(lines, str_num); // возвращаем строки без лишних пробелов
+	if (result == NULL) { // если возвращен нулевой указатель - память очистить, программу завершить
 		for (int i = 0; i <= str_num; i++) {
 			free(lines[i]);
 		}
@@ -106,15 +106,15 @@ int main(int argc, char *argv[]) {
 
 	}
 
-	for (size_t i = 0; i < str_num; i++) { // print strings
+	for (size_t i = 0; i < str_num; i++) { // вывод результирующего массива строк
 		printf("%s", result[i]);
 	}
 
-	for (size_t i = 0; i <= str_num; i++) { // clear memory
+	for (size_t i = 0; i <= str_num; i++) { // очистка входного массива строк
 		free(lines[i]);
 	}
 	free(lines);
-	for (size_t i = 0; i < str_num; i++) { // clear memory
+	for (size_t i = 0; i < str_num; i++) { // очистка результирующего массива строк
 		free(result[i]);
 	}
 	free(result);
